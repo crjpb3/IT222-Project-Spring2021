6d974d273aca993ad2fed95a8c75323f4ad33f72
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2021-04-21 16:41:48 -0500

    Update bid_db_submit.php
    Updated $_POST[''] argument names

--- bid_db_submit.php
+++ bid_db_submit.php
@@ -4,12 +4,12 @@ require_once 'connection.php';
 
 $connection = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
-if(isset($_POST['']))
+if(isset($_POST['submit']))
 {
-	$first_name = $_POST[''];
-	$last_name = $_POST[''];
-	$email = $_POST[''];
-	$bid_amount = $_POST[''];
+	$first_name = $_POST['fname'];
+	$last_name = $_POST['lname'];
+	$email = $_POST['email'];
+	$bid_amount = $_POST['bid'];
 	$query = "INSERT INTO Bids('BidderF_Name', 'BidderL_Name', 'BidderEmail', 'BidAmount') VALUES('$first_name', '$last_name', '$email', '$bid_amount')";
 	
 	if(mysqli_query($connection, $query))
